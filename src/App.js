1bbe1066c7ddef27333c4f5bc6c6574c37928214
wathsala495 <sumuduwathsala80@gmail.com>
2024-09-11 13:35:53 +0530

    form handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data,setData]=useState()
+
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(data)
+  }
+
+  const handleChange=(e)=>{
+    console.log(e.target.type)
+    console.log(e.target.name=e.target.type==='checkbox'
+      ?e.target.checked
+      :e.target.value)
+    
+      setData({...data,[e.target.name]:e.target.type==='checkbox'
+        ?e.target.checked
+        :e.target.value})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type='text' name='firstName' onChange={handleChange}/>
+          </div>
+
+          <div>
+          <input type='text' name='lasstName'onChange={handleChange}/>
+          </div>
+          <div>
+          <input type='checkbox' name='isOk' onChange={handleChange}/>
+          </div>
+          <button type='submit'>Submit</button>
+        
+      </form>
     </div>
   );
 }
